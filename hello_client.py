d39b62a0b342873115012696d3cd5abe1dcf1e73
Dennis Riungu <2971971+denpalrius@users.noreply.github.com>
2024-09-16 23:29:04 -0500

    Added rpc client

--- hello_client.py
+++ hello_client.py
@@ -1,1 +1,25 @@
-print("Niaje mzae")
+import grpc
+import proto.schema.greeter_pb2 as greeter_pb2
+import proto.schema.greeter_pb2_grpc as greeter_pb2_grpc
+
+port = 50051
+host = 'localhost'
+
+def run():
+    print('\nStarting client...')
+
+    with grpc.insecure_channel(f'{host}:{port}') as channel:
+        greeter_stub = greeter_pb2_grpc.GreeterStub(channel)
+        res1 = greeter_stub.SayHello(greeter_pb2.GreetingRequest(salutation='Mr.', name='Kababa'))
+        print("SayHello received: " + res1.message)
+
+        res2 = greeter_stub.SayHello(greeter_pb2.GreetingRequest(salutation='Miss.', name='Sandie'))
+        print("SayHello received: " + res2.message)
+        
+        res3 = greeter_stub.GetGreetingHistory(greeter_pb2.GreetingHistoryRequest(name='Mzitoh'))
+        print("GetGreetingHistory received:")
+        print('timestamp: ' + ''.join(res3.timestamp))
+        print('messages: ', res3.messages, '\n')
+
+if __name__ == '__main__':
+    run()
